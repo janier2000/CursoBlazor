2d727532943b5b6bc0eef239c3a364f42dce8233
janier2000 <jomr10@hotmail.com>
2026-05-04 18:12:25 -0500

    P5 Divida el componente de afeitar en archivos separados https://www.youtube.com/watch?v=4CeEFuY_ieg&list=PL6n9fhu94yhVowClAs8-6nYnfsOTma14P&index=5

---
+++ BlazorApplication/BlazorWebAssemblyPractice/Pages/P5 - Separar html de c shar/CountersBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebAssemblyPractice.Pages
+{
+    public class CountersBase : ComponentBase
+    {
+        protected int currentCount = 0;
+        protected string fontFamily = "Verdana";
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
