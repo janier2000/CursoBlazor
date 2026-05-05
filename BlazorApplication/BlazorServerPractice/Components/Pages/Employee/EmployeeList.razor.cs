013fd71bafb504679d8e3ea5bfb2c76de0f031b9
janier2000 <jomr10@hotmail.com>
2026-05-04 19:35:05 -0500

    parte7 Employee list blazor component https://www.youtube.com/watch?v=_vqolzW5emY&list=PL6n9fhu94yhVowClAs8-6nYnfsOTma14P&index=7

---
+++ BlazorApplication/BlazorServerPractice/Components/Pages/Employee/EmployeeList.razor.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using static Microsoft.AspNetCore.Components.Web.RenderMode;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using BlazorServerPractice;
+using BlazorServerPractice.Components;
+
+namespace EmployeeManagement.Web.Components.Pages.Employee
+{
+    public partial class EmployeeList
+    {
+
+    }
+} 
---
+++ BlazorApplication/EmployeeManagement.WebAssembly/Pages/Employee/Employee.razor.cs
@@ -0,0 +1,68 @@
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.WebAssembly.Pages.Employee
+{
+    public partial class Employee
+    {
+        private IEnumerable<Models.Employee> Employees { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            Models.Employee e1 = new Models.Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBrith = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                PhotoPath = "images/john.png"
+            };
+
+            Models.Employee e2 = new Models.Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Sam",
+                LastName = "Galloway",
+                Email = "Sam@pragimtech.com",
+                DateOfBrith = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                PhotoPath = "images/sam.jpg"
+            };
+
+            Models.Employee e3 = new Models.Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "mary@pragimtech.com",
+                DateOfBrith = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                PhotoPath = "images/mary.png"
+            };
+
+            Models.Employee e4 = new Models.Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "sara@pragimtech.com",
+                DateOfBrith = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                PhotoPath = "images/sara.png"
+            };
+
+            Employees = new List<Models.Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
