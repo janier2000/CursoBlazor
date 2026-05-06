fbeffc50b5f8de5da1685ec84c2f1ec0f5a6ced5
janier2000 <jomr10@hotmail.com>
2026-05-06 16:44:23 -0500

    .

---
+++ BlazorApplication/Serviteca.Backend/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
+
+namespace Serviteca.Backend.Data
+{
+    public class DataContext 
+    {
+
+        //public DataContext(DbContextOptions<DataContext> options) : base(options)
+        //{
+        //}
+       
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<DocumentType> DocumentTypes { get; set; }
+        //public DbSet<Vehicle> Vehicles { get; set; }
+        //public DbSet<VehicleUse> VehicleUses { get; set; }
+        //public DbSet<VehicleType> VehicleTypes { get; set; }
+        //public DbSet<VehicleBrand> VehicleBrands { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    //evita duplicados de variable (Name ) en la bd 
+        //    modelBuilder.Entity<Customer>().HasIndex(x => x.Document).IsUnique();
+        //    modelBuilder.Entity<VehicleBrand>().HasIndex(x => x.Name).IsUnique();
+        //    modelBuilder.Entity<DocumentType>().HasIndex(x => x.Name).IsUnique();
+        //    modelBuilder.Entity<VehicleType>().HasIndex(x => x.Name).IsUnique();
+        //    modelBuilder.Entity<VehicleUse>().HasIndex(x => x.Name).IsUnique();
+        //    modelBuilder.Entity<Vehicle>().HasIndex(x => x.Plate).IsUnique();
+        //}
+    }
+}
