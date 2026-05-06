647a97caed251b83ef47100fbbd3353492887fba
janier2000 <jomr10@hotmail.com>
2026-05-06 17:09:59 -0500

    .

---
+++ BlazorApplication/Serviteca.Frontend/Pages/P5 - Separar html de c shar/Counter2Base.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Serviteca.Frontend.Pages.P5___Separar_html_de_c_shar
+{
+    public class Counter2Base : ComponentBase
+    {
+        protected int currentCount = 0;
+        protected string fontFamily = "Verdana";
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
