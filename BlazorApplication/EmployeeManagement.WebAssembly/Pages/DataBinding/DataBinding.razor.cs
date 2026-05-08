a836760b1799d23959b3792c171d3d2ecaf99563
janier2000 <jomr10@hotmail.com>
2026-05-08 13:14:12 -0500

    .

---
+++ BlazorApplication/EmployeeManagement.WebAssembly/Pages/DataBinding/DataBinding.razor.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagement.WebAssembly.Pages.DataBinding
+{
+    public partial class DataBinding
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+        protected string Colour { get; set; } = "background-color:white";
+    }
+} 
