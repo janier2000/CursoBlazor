b76e411181b3e4aa4859eabaf20ab1cfac2a7a06
janier2000 <jomr10@hotmail.com>
2026-05-08 12:53:44 -0500

    .

--- BlazorApplication/EmployeeManagement.WebAssembly/Pages/Employee/EmployeeDetails.razor.cs
+++ BlazorApplication/EmployeeManagement.WebAssembly/Pages/Employee/EmployeeDetails.razor.cs
@@ -1,6 +1,7 @@
 using CurrieTechnologies.Razor.SweetAlert2;
 using EmployeeManagement.WebAssembly.Repositories;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using e = EmployeeManagement.Models;
 
 namespace EmployeeManagement.WebAssembly.Pages.Employee
@@ -8,8 +9,11 @@ namespace EmployeeManagement.WebAssembly.Pages.Employee
     public partial class EmployeeDetails
     {
         private e.Employee? employeeENT;
+        protected string Coordinates { get; set; }
         [Inject] private IRepository Repository { get; set; } = null!;
         [EditorRequired, Parameter] public int Id { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
 
         protected override async Task OnInitializedAsync()
         {
@@ -26,6 +30,25 @@ namespace EmployeeManagement.WebAssembly.Pages.Employee
                 return;
             }
             employeeENT = responseHttp.Response;
+        }
+
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        }
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
 
     }
